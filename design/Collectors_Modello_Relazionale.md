908bbbe7512f4eab60285f1164f330ce54db7d2b
Iginio Massari <94639044+darthmassari@users.noreply.github.com>
2023-05-29 21:05:43 +0200

    scambio attibuti disco in info_disco, update sql

--- design/Collectors_Modello_Relazionale.md
+++ design/Collectors_Modello_Relazionale.md
@@ -2,9 +2,9 @@
 * **Collezione** (<u>ID</u>, ID_collezionista, nome, visibilità) <br>
 * **Condivisione** (<u>ID_collezionista</u>, <u>ID_collezione</u>) <br>
 * **Artista** (<u>ID</u>, nome, tipo) <br>
-* **Disco** (<u>ID</u>, ID_autore, titolo, genere, formato, copertina) <br>
+* **Disco** (<u>ID</u>, ID_autore, titolo, formato, barcode) <br>
 * **Copia** (<u>ID_collezione</u>, <u>ID_disco</u>, stato, quantità) <br>
 * **Immagine** (<u>ID</u>, ID_disco, path, etichetta) <br>
-* **Info_Disco** (<u>ID_disco</u>, barcode, descrizione, etichetta, anno) <br>
+* **Info_Disco** (<u>ID_disco</u>, genere, descrizione, etichetta, anno, anteprima) <br>
 * **Traccia** (<u>ID</u>, ID_disco, numero, titolo, durata) <br>
 * **Collaborazione** (<u>ID_artista</u>, <u>ID_traccia</u>)
--- src/collectors_struttura.sql
+++ src/collectors_struttura.sql
@@ -42,21 +42,10 @@ CREATE TABLE disco (
     ID INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     ID_autore INTEGER UNSIGNED NOT NULL,
     titolo VARCHAR(50) NOT NULL,
-    genere VARCHAR(50),
 	formato VARCHAR(50) NOT NULL,
-    copertina BLOB,
-    CONSTRAINT generi_musicali CHECK (genere IN ('Hip-Hop' , 'R&B',
-        'Blues',
-        'Funk',
-        'Jazz',
-        'Rock',
-        'Metal',
-        'Pop',
-        'Classica',
-        'Disco',
-        'Altro')),
+	barcode VARCHAR(12),
 	CONSTRAINT formati CHECK (formato IN ("Vinile", "CD", "Digitale", "Musicassetta")),
-	CONSTRAINT disco_unico UNIQUE (ID_autore, titolo),
+	CONSTRAINT disco_unico UNIQUE (ID_autore, titolo, barcode),
     CONSTRAINT autore_disco FOREIGN KEY (ID_autore)
         REFERENCES artista (ID)
         ON DELETE NO ACTION ON UPDATE CASCADE
@@ -68,14 +57,13 @@ CREATE TABLE copia (
     quantita SMALLINT NOT NULL DEFAULT 1,
 	stato VARCHAR(50) NOT NULL,
     CONSTRAINT disco_unico UNIQUE (ID_collezione , ID_disco),
-    CONSTRAINT stati_conservazione CHECK (stato IN ("Nuovo", "Come nuovo", "Buono", "Difetti", "n/a")),
+    CONSTRAINT stati_conservazione CHECK (stato IN ("Nuovo", "Come nuovo", "Buono", "Pessimo", "n/a")),
     CONSTRAINT collezione_copia FOREIGN KEY (ID_collezione)
         REFERENCES collezione (ID)
-        ON DELETE CASCADE ON UPDATE CASCADE,
+        ON DELETE NO ACTION ON UPDATE CASCADE,
     CONSTRAINT disco_copia FOREIGN KEY (ID_disco)
-        REFERENCES disco (ID
-        -- cascade?
-        ON DELETE NO ACTION ON UPDATE CASCADE
+        REFERENCES disco (ID)
+        ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE immagine (
@@ -90,10 +78,21 @@ CREATE TABLE immagine (
 
 CREATE TABLE info_disco (
     ID_disco INTEGER UNSIGNED NOT NULL UNIQUE,
-	barcode VARCHAR(12),
+	genere VARCHAR(50),
     descrizione VARCHAR(5000),
     etichetta VARCHAR(50),
     anno SMALLINT UNSIGNED,
+	anteprima BLOB,
+    CONSTRAINT generi_musicali CHECK (genere IN ('Hip-Hop' , 'R&B',
+        'Blues',
+        'Funk',
+        'Jazz',
+        'Rock',
+        'Metal',
+        'Pop',
+        'Classica',
+        'Disco',
+        'Altro')),
     CONSTRAINT controllo_anno CHECK (anno > 1900 AND anno < 2100),
     CONSTRAINT disco_dettagli FOREIGN KEY (ID_disco)
         REFERENCES disco (ID)
@@ -104,9 +103,10 @@ CREATE TABLE traccia (
     ID INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     ID_disco INTEGER UNSIGNED NOT NULL,
     numero TINYINT NOT NULL,
-    titolo VARCHAR(50) NOT NULL UNIQUE,
+    titolo VARCHAR(50) NOT NULL,
     durata TIME,
     CONSTRAINT controllo_durata CHECK (durata < '00:30:00'),
+    CONSTRAINT traccia_unica UNIQUE (ID_disco, numero),
     CONSTRAINT disco_traccia FOREIGN KEY (ID_disco)
         REFERENCES disco (ID)
         ON DELETE NO ACTION ON UPDATE CASCADE
